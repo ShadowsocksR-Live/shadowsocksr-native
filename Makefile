ada32ad1c0fa1682d0a2804b870a01ba25084749
clowwindy <clowwindy42@gmail.com>
2012-05-24 12:00:02 +0800

    use autoconf

--- Makefile
+++
@@ -1,8 +0,0 @@
-main: main.o
-	gcc -g -O2 main.o -lev -lssl -L/usr/local/lib -o main
-main.o: *.c *.h Makefile
-	gcc -g -O2 -c -o main.o main.c
-.PHONY: clean
-clean:
-	rm *.o main
-
---
+++ encrypt.h
@@ -0,0 +1,17 @@
+#ifndef ENCRYPT_H
+#define ENCRYPT_H
+
+#include <sys/socket.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+unsigned char encrypt_table[256];
+unsigned char decrypt_table[256];
+void get_table(const unsigned char* key);
+void encrypt(char *buf, int len);
+void decrypt(char *buf, int len);
+int send_encrypt(int sock, char *buf, int len, int flags);
+int recv_decrypt(int sock, char *buf, int len, int flags);
+
+#endif // ENCRYPT_H
