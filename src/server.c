c2a108cc888bc1e0cf28cd630c4f94a0d2692016
Max Lv <max.c.lv@gmail.com>
2013-06-15 11:27:42 +0800

    fix the ntop

--- src/server.c
+++ src/server.c
@@ -221,7 +221,7 @@ static void server_recv_cb (EV_P_ ev_io *w, int revents) {
             size_t in_addr_len = sizeof(struct in_addr);
             if (r > in_addr_len) {
                 inet_ntop(AF_INET, (void *)(server->buf + offset),
-                        host, in_addr_len);
+                        host, INET_ADDRSTRLEN);
                 offset += in_addr_len;
             }
 
@@ -240,7 +240,7 @@ static void server_recv_cb (EV_P_ ev_io *w, int revents) {
             size_t in6_addr_len = sizeof(struct in6_addr);
             if (r > in6_addr_len) {
                 inet_ntop(AF_INET6, (const void*)(server->buf + offset), 
-                        host, in6_addr_len);
+                        host, INET6_ADDRSTRLEN);
                 offset += in6_addr_len;
             }
 
