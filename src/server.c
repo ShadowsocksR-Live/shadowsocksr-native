3511f23d0cb98e9f1154b7211a99bd0123665f17
Max Lv <max.c.lv@gmail.com>
2014-04-18 16:08:23 +0800

    tune resolve timer

--- src/server.c
+++ src/server.c
@@ -793,7 +793,7 @@ struct server* new_server(int fd, struct listen_ctx *listener)
     server->fd = fd;
     ev_io_init(&server->recv_ctx->io, server_recv_cb, fd, EV_READ);
     ev_io_init(&server->send_ctx->io, server_send_cb, fd, EV_WRITE);
-    ev_timer_init(&server->send_ctx->watcher, server_resolve_cb, 0.2, 0.5);
+    ev_timer_init(&server->send_ctx->watcher, server_resolve_cb, 0.1, 0.2);
     ev_timer_init(&server->recv_ctx->watcher, server_timeout_cb, listener->timeout, listener->timeout * 5);
     server->recv_ctx->server = server;
     server->recv_ctx->connected = 0;
