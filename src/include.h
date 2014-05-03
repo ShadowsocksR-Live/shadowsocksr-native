dbdb4e03a0eace5947f91726cf0828a48f3469a9
Max Lv <max.c.lv@gmail.com>
2014-05-02 23:15:18 -0400

    add TCP_FASTOPEN macros

--- src/include.h
+++ src/include.h
@@ -1,6 +1,16 @@
 #ifndef _INCLUDE_H
 #define _INCLUDE_H
 
+/*  conditional define for TCP_FASTOPEN */
+#ifndef TCP_FASTOPEN
+#define TCP_FASTOPEN   23
+#endif
+ 
+/*  conditional define for MSG_FASTOPEN */
+#ifndef MSG_FASTOPEN
+#define MSG_FASTOPEN   0x20000000
+#endif
+
 int udprelay_init(const char *server_host, const char *server_port,
 #ifdef UDPRELAY_LOCAL
                   const char *remote_host, const char *remote_port,
