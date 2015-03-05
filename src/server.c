c760f11902b51da1bcf4f2df179bc47b5e5ee679
Max Lv <mlv@nvidia.com>
2015-03-05 12:05:53 +0800

    refine #274

--- src/server.c
+++ src/server.c
@@ -177,12 +177,12 @@ int create_and_bind(const char *host, const char *port)
     hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG; /* For wildcard IP address */
     hints.ai_protocol = IPPROTO_TCP;
 
-    for (int i = 0; i < 8; i++) {
+    for (int i = 1; i < 8; i++) {
         s = getaddrinfo(host, port, &hints, &result);
         if (s == 0) break;
         else {
             sleep(pow(2, i));
-            LOGE("failed to resolve server name, wait for %.0fs", pow(2, i));
+            LOGE("failed to resolve server name, wait %.0f seconds", pow(2, i));
         }
 
     }
