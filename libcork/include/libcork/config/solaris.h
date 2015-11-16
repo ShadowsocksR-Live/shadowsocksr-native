e4c0bc9d2716a7e67a1b710be9b986e8aea63c6c
Max Lv <mlv@nvidia.com>
2015-11-16 12:22:49 +0800

    use the proper header for solaris

--- libcork/include/libcork/config/solaris.h
+++ libcork/include/libcork/config/solaris.h
@@ -15,12 +15,12 @@
  * Endianness
  */
 
-#include <sys/endian.h>
+#include <sys/isa_defs.h>
 
-#if BYTE_ORDER == BIG_ENDIAN
+#if defined(_BIG_ENDIAN)
 #define CORK_CONFIG_IS_BIG_ENDIAN      1
 #define CORK_CONFIG_IS_LITTLE_ENDIAN   0
-#elif BYTE_ORDER == LITTLE_ENDIAN
+#elif defined(_LITTLE_ENDIAN)
 #define CORK_CONFIG_IS_BIG_ENDIAN      0
 #define CORK_CONFIG_IS_LITTLE_ENDIAN   1
 #else
