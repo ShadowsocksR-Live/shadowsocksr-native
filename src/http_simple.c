63f357a4b507364692bec02ecf82f056376dd663
破娃酱 <mmgac001@gmail.com>
2016-08-07 16:39:39 +0800

    random useragent, custom http head

--- src/http_simple.c
+++ src/http_simple.c
@@ -1,6 +1,23 @@
 
 #include "http_simple.h"
 
+static char* g_useragent[] = {
+    "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
+    "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:40.0) Gecko/20100101 Firefox/44.0",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Ubuntu/11.10 Chromium/27.0.1453.93 Chrome/27.0.1453.93 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0",
+    "Mozilla/5.0 (compatible; WOW64; MSIE 10.0; Windows NT 6.2)",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.3; Trident/7.0; .NET4.0E; .NET4.0C)",
+    "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko",
+    "Mozilla/5.0 (Linux; Android 4.4; Nexus 5 Build/BuildID) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36",
+    "Mozilla/5.0 (iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3",
+};
+
+static int g_useragent_index = -1;
+
 typedef struct http_simple_local_data {
     int has_sent_header;
     int has_recv_header;
@@ -11,6 +28,10 @@ void http_simple_local_data_init(http_simple_local_data* local) {
     local->has_sent_header = 0;
     local->has_recv_header = 0;
     local->encode_buffer = NULL;
+
+    if (g_useragent_index == -1) {
+        g_useragent_index = xorshift128plus() % (sizeof(g_useragent) / sizeof(*g_useragent));
+    }
 }
 
 obfs * http_simple_new_obfs() {
@@ -62,6 +83,7 @@ int http_simple_client_encode(obfs *self, char **pencryptdata, int datalength, s
     int head_size = self->server.head_len + (xorshift128plus() & 0x3F);
     int outlength;
     char * out_buffer = (char*)malloc(datalength + 2048);
+    char * body_buffer = NULL;
     if (head_size > datalength)
         head_size = datalength;
     http_simple_encode_head(local, encryptdata, head_size);
@@ -72,6 +94,35 @@ int http_simple_client_encode(obfs *self, char **pencryptdata, int datalength, s
     for (pos = 0; hosts[pos]; ++pos) {
         if (hosts[pos] == ',') {
             phost[host_num++] = &hosts[pos + 1];
+            hosts[pos] = 0;
+        } else if (hosts[pos] == '#') {
+            char * body_pointer = &hosts[pos + 1];
+            char * p;
+            int trans_char = 0;
+            p = body_buffer = (char*)malloc(2048);
+            for ( ; *body_pointer; ++body_pointer) {
+                if (*body_pointer == '\\') {
+                    trans_char = 1;
+                    continue;
+                }
+                if (trans_char) {
+                    if (*body_pointer == '\\' ) {
+                        *p = '\\';
+                    } else if (*body_pointer == 'n' ) {
+                        *p = '\r';
+                        *++p = '\n';
+                    } else {
+                        *p = '\\';
+                        *p = *body_pointer;
+                    }
+                    trans_char = 0;
+                } else {
+                    *p = *body_pointer;
+                }
+                ++p;
+            }
+            hosts[pos] = 0;
+            break;
         }
     }
     host_num = xorshift128plus() % host_num;
@@ -79,7 +130,16 @@ int http_simple_client_encode(obfs *self, char **pencryptdata, int datalength, s
         sprintf(hostport, "%s", phost[host_num]);
     else
         sprintf(hostport, "%s:%d", phost[host_num], self->server.port);
-    sprintf(out_buffer,
+    if (body_buffer) {
+        sprintf(out_buffer,
+            "GET /%s HTTP/1.1\r\n"
+            "Host: %s\r\n"
+            "%s\r\n\r\n",
+            local->encode_buffer,
+            hostport,
+            body_buffer);
+    } else {
+        sprintf(out_buffer,
             "GET /%s HTTP/1.1\r\n"
             "Host: %s\r\n"
             "User-Agent: %s\r\n"
@@ -91,8 +151,9 @@ int http_simple_client_encode(obfs *self, char **pencryptdata, int datalength, s
             "\r\n",
             local->encode_buffer,
             hostport,
-            "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0"
+            g_useragent[g_useragent_index]
             );
+    }
     outlength = strlen(out_buffer);
     memmove(out_buffer + outlength, encryptdata + head_size, datalength - head_size);
     outlength += datalength - head_size;
@@ -103,6 +164,8 @@ int http_simple_client_encode(obfs *self, char **pencryptdata, int datalength, s
     }
     memmove(encryptdata, out_buffer, outlength);
     free(out_buffer);
+    if (body_buffer != NULL)
+        free(body_buffer);
     if (local->encode_buffer != NULL) {
         free(local->encode_buffer);
         local->encode_buffer = NULL;
