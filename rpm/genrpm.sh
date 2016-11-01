d6c377df9c8053b10276dd2dddf9aceda5f1faf5
Max Lv <max.c.lv@gmail.com>
2016-11-01 09:56:22 +0800

    Bump version

--- rpm/genrpm.sh
+++ rpm/genrpm.sh
@@ -7,7 +7,7 @@ show_help()
     echo
     echo -e "Options:"
     echo -e "  -h    show this help."
-    echo -e "  -v    with argument version (2.5.5 by default)."
+    echo -e "  -v    with argument version (2.5.6 by default)."
     echo -e "  -f    with argument format (tar.xz by default) used by git archive."
     echo
     echo -e "Examples:"
@@ -38,7 +38,7 @@ do
     esac
 done
 
-: ${version:=2.5.5}
+: ${version:=2.5.6}
 : ${format:=tar.gz}
 
 name="shadowsocks-libev"
