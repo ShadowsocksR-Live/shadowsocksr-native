854c6f9779ee41bc6b3e895b4dbf30a5d13e0b88
Max Lv <max.c.lv@gmail.com>
2016-11-07 17:57:00 +0800

    Fix build with clang

--- src/acl.c
+++ src/acl.c
@@ -53,16 +53,18 @@ static int
 run_cmd(const char *cmdstring)
 {
     pid_t pid;
-    int status;
-    if (cmdstring == NULL) {
-        return 1;
-    }
+    int status = 0;
+
+    if (cmdstring == NULL)
+        return -1;
+
     if ((pid = fork()) < 0) {
         status = -1;
     } else if (pid == 0) {
         execl("/bin/sh", "sh", "-c", cmdstring, (char *)0);
         _exit(127);
     }
+
     return status;
 }
 
