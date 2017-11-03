1624d6d63f95d9c084d987e826e08e2018c91381
ssrlive <ssrlivebox@gmail.com>
2017-11-03 10:22:11 +0800

    ssrcipher.c/h

---
+++ src/client/ssrcipher.h
@@ -0,0 +1,12 @@
+#if !defined(__SSR_CIPHER__)
+#define __SSR_CIPHER__ 1
+
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+#endif // defined(__SSR_CIPHER__)
