b1647d7229bc676273442030c10b42de7b960038
ssrlive <ssrlivebox@gmail.com>
2017-12-08 01:58:25 +0800

    remove old libsodium code.

--- libsodium/test/HAVE_WEAK_SYMBOLS.c
+++
@@ -1,2 +0,0 @@
-__attribute__((weak)) void __dummy(void *x) { }
-void f(void *x) { __dummy(x); }
--- libsodium/test/IS_STDC_LIMIT_MACROS_NEEDED.c
+++
@@ -1,7 +0,0 @@
-#include <limits.h>
-#include <stdint.h>
-
-void main(){
-	(void) SIZE_MAX;
-	(void) UINT64_MAX;
-}
