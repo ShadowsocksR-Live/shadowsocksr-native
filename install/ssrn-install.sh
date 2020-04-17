b48aa6bb6c4e391d71fb54489f327bf4f6ac5ec1
ssrlive <30760636+ssrlive@users.noreply.github.com>
2020-04-17 13:33:35 +0800

    Update ssrn-install.sh

--- install/ssrn-install.sh
+++ install/ssrn-install.sh
@@ -342,16 +342,16 @@ function install_build_tools() {
 
     # Install necessary dependencies
     if check_sys packageManager yum; then
-        yum install wget curl git gcc gcc-c++ autoconf automake libtool make asciidoc xmlto -y
+        yum install wget curl git gcc gcc-c++ gdb autoconf automake libtool make asciidoc xmlto -y
     elif check_sys packageManager apt; then
         apt-get -f install -y
         apt-get -y update
         apt-get -y upgrade
         apt-get -y install --no-install-recommends build-essential autoconf libtool asciidoc xmlto
-        apt-get -y install git gcc g++ wget curl automake
+        apt-get -y install git gcc g++ gdb wget curl automake
     fi
 
-    curl https://cmake.org/files/v3.12/cmake-3.12.1-Linux-x86_64.sh -o cmake_pkg.sh
+    curl https://cmake.org/files/v3.17/cmake-3.17.0-Linux-x86_64.sh -o cmake_pkg.sh
     sh cmake_pkg.sh --prefix=/usr/ --exclude-subdir && rm -rf cmake_pkg.sh
 }
 
