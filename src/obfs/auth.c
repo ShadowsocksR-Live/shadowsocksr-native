2bea1e1fadadd85497632d20e36e6d1bc55f121e
ssrlive <ssrlivebox@gmail.com>
2020-04-28 09:43:43 +0000

    warnning error

--- src/obfs/auth.c
+++ src/obfs/auth.c
@@ -1560,10 +1560,13 @@ struct buffer_t * auth_aes128_sha1_server_post_decrypt(struct obfs_t *obfs, stru
             return buffer_create(1);
         }
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstrict-aliasing"
         utc_time = (uint32_t) (*((uint32_t *)(head + 0))); // TODO: ntohl
         client_id = (uint32_t) (*((uint32_t *)(head + 4))); // TODO: ntohl
         connection_id = (uint32_t) (*((uint32_t *)(head + 8))); // TODO: ntohl
         rnd_len = (uint16_t) (*((uint16_t *)(head + 14))); // TODO: ntohs
+#pragma GCC diagnostic pop
         {
             struct buffer_t *_msg = buffer_create_from(buffer_get_data(local->recv_buffer, NULL), length-4);
             local->hmac(sha1data, _msg, local->user_key);
