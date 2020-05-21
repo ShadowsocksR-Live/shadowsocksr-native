02f6d53257e7436a2ad5608b9c197b350dcb3516
ssrlive <ssrlivebox@gmail.com>
2020-05-21 09:41:39 +0800

    force_quit

--- src/client/ssr_client_api.h
+++ src/client/ssr_client_api.h
@@ -12,6 +12,7 @@ struct ssr_client_state;
 int ssr_run_loop_begin(struct server_config *cf, void(*feedback_state)(struct ssr_client_state *state, void *p), void *p);
 void ssr_run_loop_shutdown(struct ssr_client_state *state);
 int ssr_get_listen_socket_fd(struct ssr_client_state *state);
+void state_set_force_quit(struct ssr_client_state *state, bool force_quit);
 
 void set_app_name(const char *name);
 void set_dump_info_callback(void(*callback)(const char *info, void *p), void *p);
