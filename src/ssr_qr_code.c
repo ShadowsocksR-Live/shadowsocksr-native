f317f0829de299585e521ea5119bfdc6b7201b29
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-01-02 20:42:14 +0800

    generate Shadowsocks style QR code.

--- src/ssr_qr_code.c
+++ src/ssr_qr_code.c
@@ -3,6 +3,8 @@
 #include "ssr_executive.h"
 #include "ssr_cipher_names.h"
 #include "obfs.h" // for SSR_BUFF_SIZE
+#include <uri_encode.h>
+#include <stdbool.h>
 
 static const char *ss_header = "ss://";
 static const char *ssr_header = "ssr://";
@@ -16,6 +18,7 @@ static const char *ot_enable = "ot_enable";
 static const char *ot_domain = "ot_domain";
 static const char *ot_path = "ot_path";
 
+char *generate_shadowsocks_uri(const struct server_config *config, void*(*alloc_fn)(size_t size));
 
 char * ssr_qr_code_encode(const struct server_config *config, void*(*alloc_fn)(size_t size)) {
     unsigned char *base64_buf;
@@ -36,6 +39,12 @@ char * ssr_qr_code_encode(const struct server_config *config, void*(*alloc_fn)(s
         config->protocol == NULL ||
         config->obfs == NULL) {
         return NULL;
+    }
+
+    if (ssr_protocol_type_of_name(config->protocol) == ssr_protocol_origin &&
+        ssr_obfs_type_of_name(config->obfs) == ssr_obfs_plain)
+    {
+        return generate_shadowsocks_uri(config, alloc_fn);
     }
 
     // ssr://base64(host:port:protocol:method:obfs:base64pass/?obfsparam=base64param&protoparam=base64param&remarks=base64remarks&group=base64group&udpport=0&uot=0&ot_enable=0&ot_domain=base64domain&ot_path=base64path)
@@ -105,6 +114,44 @@ char * ssr_qr_code_encode(const struct server_config *config, void*(*alloc_fn)(s
     return result;
 }
 
+#define PORT_STR_LENGTH_MAX 5
+
+char *generate_shadowsocks_uri(const struct server_config *config, void*(*alloc_fn)(size_t size)) {
+    size_t len;
+    char *userinfo, *userinfo_b64, *uri_remarks = NULL, *res;
+    if (ssr_protocol_type_of_name(config->protocol) != ssr_protocol_origin ||
+        ssr_obfs_type_of_name(config->obfs) != ssr_obfs_plain)
+    {
+        return NULL;
+    }
+    userinfo = (char*)calloc(strlen(config->method) + PORT_STR_LENGTH_MAX + 2, sizeof(char));
+    sprintf(userinfo, "%s:%d", config->method, config->remote_port);
+
+    userinfo_b64 = url_safe_base64_encode_alloc((uint8_t*)userinfo, strlen(userinfo), &malloc);
+
+    if (config->remarks && strlen(config->remarks)) {
+        size_t u_len = URI_ENCODE_BUFF_SIZE_MAX(strlen(config->remarks));
+        uri_remarks = (char *)calloc(u_len, sizeof(char));
+        uri_encode(config->remarks, strlen(config->remarks), uri_remarks, u_len);
+    }
+    // plugins not supported.
+    len = 5 + strlen(userinfo_b64) + 1 + strlen(config->remote_host) + 1 + PORT_STR_LENGTH_MAX + (uri_remarks?(1 + strlen(uri_remarks)):0) + 1;
+
+    res = (char *) alloc_fn(len);
+    memset(res, 0, len);
+    if (uri_remarks) {
+        sprintf(res, "%s%s@%s:%d#%s", ss_header, userinfo_b64, config->remote_host, config->remote_port, uri_remarks);
+    } else {
+        sprintf(res, "%s%s@%s:%d", ss_header, userinfo_b64, config->remote_host, config->remote_port);
+    }
+
+    free(uri_remarks);
+    free(userinfo);
+    free(userinfo_b64);
+
+    return res;
+}
+
 struct server_config * decode_shadowsocks(const char *text);
 struct server_config * decode_ssr(const char *text);
 
@@ -136,13 +183,14 @@ struct server_config * decode_shadowsocks(const char *text) {
 
     do {
         size_t hdr_len;
-        char *remarks;
-        int len;
-        char *method;
-        char *password;
-        char *port;
-        char *hostname;
-        const char *t;
+        char *remarks = NULL;
+        int len = 0;
+        char *method = NULL;
+        char *password = NULL;
+        char *port = NULL;
+        char *hostname = NULL;
+        const char *t = NULL;
+        bool is_aead = false;
 
         if (text == NULL || strlen(text)==0) {
             break;
@@ -162,9 +210,43 @@ struct server_config * decode_shadowsocks(const char *text) {
         }
 
         if (strcspn(contents, "@:/?") != strlen(contents)) {
-            // SS AEAD not support forever.
-            break;
-        }
+            //
+            // SS-URI = "ss://" userinfo "@" hostname ":" port [ "/" ] [ "?" plugin ] [ "#" tag ]
+            // userinfo = websafe-base64-encode-utf8(method  ":" password)
+            //
+            // ss://YmYtY2ZiOnRlc3Q@192.168.100.1:8888/?plugin=url-encoded-plugin-argument-value&unsupported-arguments=should-be-ignored#Dummy+profile+name.
+            //
+            char *plugin = strstr(contents, "/?");
+            is_aead = true;
+            if (plugin) {
+                *plugin = '\0';
+                printf("%s", "Don't support plugin features.");
+                // break;
+            }
+            hostname = strchr(contents, '@');
+            if (hostname == NULL) {
+                break;
+            }
+            *hostname++ = '\0';
+
+            port = strrchr(hostname, ':');
+            if (port == NULL) {
+                break;
+            }
+            *port++ = '\0';
+
+            len = (int) url_safe_base64_decode_len(contents);
+            plain_text = (char *) calloc(len+1, sizeof(plain_text[0]));
+            url_safe_base64_decode(contents, (uint8_t *)plain_text);
+
+            password = strchr(plain_text, ':');
+            if (password == NULL) {
+                break;
+            }
+            *password++ = '\0';
+
+            method = plain_text;
+        } else {
 
         len = (int) url_safe_base64_decode_len((unsigned char *)contents);
         plain_text = (char *) calloc(len+1, sizeof(plain_text[0]));
@@ -190,14 +272,33 @@ struct server_config * decode_shadowsocks(const char *text) {
         }
         *hostname++ = '\0';
 
+        if (ss_cipher_type_of_name(method) >= ss_cipher_aes_128_gcm) {
+            is_aead = true;
+        }
+
+        } // end of else
+
+        if (isdigit(port[0]) == 0) {
+            break;
+        }
+
         config = config_create();
         string_safe_assign(&config->method, method);
         string_safe_assign(&config->password, password);
         string_safe_assign(&config->remote_host, hostname);
         config->remote_port = atoi(port);
         if (remarks) {
+            if (is_aead) {
+                size_t t_len = strlen(remarks) + 1;
+                char *tmp = (char*)calloc(t_len, sizeof(char));
+                uri_decode(remarks, tmp, t_len);
+                string_safe_assign(&config->remarks, tmp);
+                free(tmp);
+            } else {
             string_safe_assign(&config->remarks, remarks);
+            }
         }
+        string_safe_assign(&config->over_tls_path, "");
 
         t = ssr_protocol_name_of_type(ssr_protocol_origin);
         string_safe_assign(&config->protocol, t);
