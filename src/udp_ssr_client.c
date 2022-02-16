df0eb7182d91ce30f69c0c96f2f98763375b58dd
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-02-16 22:35:16 +0800

    _MSC_VER issue.

--- src/udp_ssr_client.c
+++ src/udp_ssr_client.c
@@ -22,7 +22,7 @@
 #include "config.h"
 #endif
 
-#if defined(HAVE_SYS_IOCTL_H) && defined(HAVE_NET_IF_H) && defined(__linux__)
+#if defined(HAVE_SYS_IOCTL_H) && defined(HAVE_NET_IF_H) && defined(__linux__) && !defined(_MSC_VER)
 #include <net/if.h>
 #include <sys/ioctl.h>
 #define SET_INTERFACE
