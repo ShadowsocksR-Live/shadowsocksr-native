3721e38461dfabddb4e992179db6d492674852a5
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-07-19 18:55:01 +0800

    Update config_json.c

--- src/config_json.c
+++ src/config_json.c
@@ -142,12 +142,9 @@ struct server_config* parse_config_file(bool is_server, const char* file)
                 continue;
             }
 
-            {
-            char udp[10] = UDP_STR;
-            if (json_iter_extract_bool(strlwr(udp), &iter, &obj_bool)) {
+            if (json_iter_extract_bool("udp", &iter, &obj_bool)) {
                 config->udp = obj_bool;
                 continue;
-            }
             }
             if (json_iter_extract_int("idle_timeout", &iter, &obj_int)) {
                 config->idle_timeout = obj_int * MILLISECONDS_PER_SECOND;
